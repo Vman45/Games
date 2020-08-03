493f16039c2e3f5849222cf2862fb7f20620e1dc
Artur Malisevych <19artur98@gmail.com>
2020-08-03 18:14:52 +0200

    Some logic to love form

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,8 +1,20 @@
 var calculate  = document.getElementById("calculate");
 
 function calculator(){
+    var you = document.getElementById("you").value;
+    var she = document.getElementById("she").value;
+    var loveForm = document.getElementById("loveForm");
     var love = Math.floor(Math.random()*100+1);
-    console.log(love)
+    if( you==="" && she===""){
+        alert("Please enter NAMES ")
+    }else if(you===""){
+        alert("Please enter YOUR NAME")
+    }else if(she===""){
+        alert("Please enter HER/HIS NAME")
+    }else{
+        console.log(love);
+        loveForm.reset()
+    };
 };
 
 calculate.addEventListener('click',function (e){
