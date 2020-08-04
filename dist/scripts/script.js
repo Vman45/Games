714532077ad3057d64a9d06718d36733607be54e
Artur Malisevych <19artur98@gmail.com>
2020-08-04 11:25:54 +0200

    Basic behavior love page

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,10 +1,15 @@
 var calculate  = document.getElementById("calculate");
+var tryAgain = document.getElementById("tryAgain");
 
 function calculator(){
     var you = document.getElementById("you").value;
     var she = document.getElementById("she").value;
     var loveForm = document.getElementById("loveForm");
+    var loveDescription = document.getElementById("loveDescription");
+    var lovePercentage = document.getElementById("lovePercentage");
     var love = Math.floor(Math.random()*100+1);
+
+
     if( you==="" && she===""){
         alert("Please enter NAMES ")
     }else if(you===""){
@@ -12,7 +17,14 @@ function calculator(){
     }else if(she===""){
         alert("Please enter HER/HIS NAME")
     }else{
-        console.log(love);
+        
+        lovePercentage.innerText = `${love}%`;
+        loveDescription.style.display = "none";
+        lovePercentage.style.height = "100%"
+        tryAgain.style.height = "100%"
+        tryAgain.style.display = "block";
+        lovePercentage.style.display = "block";
+
         loveForm.reset()
     };
 };
@@ -20,4 +32,10 @@ function calculator(){
 calculate.addEventListener('click',function (e){
     e.preventDefault();
     calculator()
+})
+tryAgain.addEventListener('click',function(){
+    loveDescription.style.display = "block";
+    tryAgain.style.display = "none";
+    lovePercentage.style.display = "none";
+    lovePercentage.innerText = "";
 })
